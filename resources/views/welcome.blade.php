f97b8d4e5deb2e4d6d45a856ff873aa8a8037b68
Jonathan Mann <jonathan@rallio.com>
2018-09-07 10:22:52 -0500

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Bootstrap CSS -->
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+         <style media="screen">
+            header {
+              background-color: black;
+            }
+
+            header nav {
+                background-image: url('https://d9i5ve8f04qxt.cloudfront.net/10259/12317/themes/Woodland/theme/assets/imgs/header_texture-2.jpg');
+            }
+
+            a {
+              color: #c0bfbf;
+            }
+
+            a:hover {
+              color: #c0bfbf;
+              text-shadow: 0.5px 0.5px 0px white, 0 0 25px white, 0 0 5px white;
+            }
+         </style>
+    </head>
+    <body>
+      <header>
+        <nav class="navbar navbar-expand-lg">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#"><img src="http://www.gracieacademy.com/images/landingpage/logo.gif" alt=""></a>
+            <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">The Academy</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Training Programs</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Certified Training Centers</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Certified Belt Holders</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Gracie News</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Gracie Store</a>
+              </li>
+            </ul>
+
+          </div>
+        </nav>
+      </header>
+    </body>
+</html>
